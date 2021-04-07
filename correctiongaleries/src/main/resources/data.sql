92e30f618d5ef056c2dac372494f6bb3f4e820b2
bdacosta <bdacosta@cst-tp-b10516>
2021-04-07 14:45:41 +0200

    bdd faite

--- correctiongaleries/src/main/resources/data.sql
+++ correctiongaleries/src/main/resources/data.sql
@@ -1,35 +1,25 @@
 -- Les données qui seront initialisées automatiquement quand on lance l'application
 -- cf. application.properties
-INSERT INTO Galerie(id, nom, adresse) VALUES 
-    (1, 'Saatchi', 'King''s Road, Londres'),
-    (2, 'Christies', 'Saville Road, Londres');
+INSERT INTO Compte(DTYPE,id, nom, prenom, adresse, ville, numtel, email,mdp) VALUES 
+    ('Medecin','WassLaMenace', 'Aityoucef', 'Wassim','24 rue de la Tolosane','81100 Castres','0782350075','wass.aityoucef@gmail.com','pipicacaproutmdr'),
+    ('Medecin','RemiBastide', 'Bastide', 'Remi','2 rue de la Paix','81000 Albi','0606060606','remibastide@gmail.com','remibastide'),
+    ('Patient','narcolin', 'Gay', 'Colin','11 place de la République','81100 Castres','0783734322','colin.gay25@gmail.com','jesuisGAYcolin'),
+    ('Patient','flotov', 'Thauvin', 'Florian','51 rue du Pastis','13000 Marseille','0613131313','allezlom@bouillabaisse.com','velodrome13pastis'),
+    ('Patient','pbernardoni', 'Bernardoni', 'Paul','17 avenue De Gaulle','75000 Paris','0610203040','pbernardoni@gmail.com','pauldu75leboss'),
+    ('Patient','antoinevidal', 'Vidal', 'Antoine','2 rue Beaujeu','81100 Castres','0622334455','antovidal@gmail.com','leroymerlin');
 
-INSERT INTO Exposition(id, intitule, debut, duree, organisateur_id) VALUES
-    (1, 'Painters'' painters', NOW(), 10, 1),
-    (2, 'From Selfie to Self-Expression', NOW(), 10, 1);
+INSERT INTO Patient(DTYPE,numSecu) VALUES
+    ('101026912312312'),
+    ('193011348648755'),
+    ('198077578943488'),
+    ('187068186734057');
 
--- On a choisi la stratégie 'SINGLE_TABLE' pour l'héritage
-INSERT INTO Personne(DTYPE, id, nom, adresse, biographie) VALUES
-    ('Personne', 1, 'Rémi Bastide', 'ISIS Castres', null),
-    ('Artiste',  2, 'Pablo Picasso', null, 'Peintre Catalan'),
-    ('Artiste',  3, 'Cy Twombly', null, 'né Edwin Parker Twombly Jr. le 25 avril 1928 à Lexington – mort le 5 juillet 2011 à Rome, est un peintre, dessinateur, sculpteur et photographe américain');
+INSERT INTO Médecin(numPPS, specialite,diplome) VALUES
+    ('15448','Psychologie',TRUE),
+    ('45645','Psychothérapie', TRUE),
 
-INSERT INTO Tableau(id, titre, support, dimension, auteur_id) VALUES
-    (1, 'Guernica', 'huile sur toile', '4 m.50 x 3 m. 65', 2),
-    (2, 'Corrida', 'huile sur toile','2 m.50 x 1 m. 65', 2),
-    (3, 'Tiznit',  'Acrylique sur bois','12 m x 5 m', 3),
-    (4, 'Lepanto, Part XII', 'Acrylique sur bois', '12 m x 5 m', 3),
-    (5, 'Vierge à l''enfant', 'Huile sur bois', '120cm x 60 cm', NULL);
-
-INSERT INTO Exposition_Oeuvres(accrochages_id, oeuvres_id) VALUES
-    (1, 1),
-    (1, 2),
-    (1, 3),
-    (1, 4),
-    (2, 2),
-    (2, 3);
-
-INSERT INTO Transaction(client_id, lieu_de_vente_id, oeuvre_id, prix_vente, vendu_le) VALUES
-    (1, 1, 1, 1000.01, NOW()),
-    (1, 1, 4, 2000.02, NOW()),
-    (1, 2, 3, 3000.03, NOW());
+INSERT INTO Bracelet(numSerie, freqcard, coordGPS) VALUES
+    ('DBZ831235','120','39° 17′ N, 76° 36′ O'),
+    ('SNK943534','120','41° 22′ N, 70° 11′ O'),
+    ('JJK545307','120','30° 31′ N, 79° 33′ O'),
+    ('MHA815615','120','37° 26′ N, 69° 19′ O');
